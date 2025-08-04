b958e69222ffa05ada26dccfb1e50c1c7dce6f41
Рамзиддин Эргашалиев <rergashaliev@trtparts.com>
2025-08-04 08:59:00 +0500

    add change lenguage

--- src/components/Application/Application.jsx
+++ src/components/Application/Application.jsx
@@ -1,22 +1,3 @@
-// import React from "react";
-// import "./application.scss";
-
-// const Application = ({ data }) => {
-//   return (
-//     <div className="application">
-//       {data?.model?.map((modelName, index) => (
-//         <div className="application__card" key={index}>
-//           <p className="application__text">{data?.carName?.[0]}</p>
-//           <h3 className="application__title">{modelName}</h3>
-//           <p className="application__desc">{data?.year}</p>
-//         </div>
-//       ))}
-//     </div>
-//   );
-// };
-
-// export default Application;
-
 import React from "react";
 import "./application.scss";
 
