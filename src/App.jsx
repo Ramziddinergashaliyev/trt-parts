0e87a7279e637607ccbb3929707c026aa7a9f693
Ramziddinergashaliyev <155714547+Ramziddinergashaliyev@users.noreply.github.com>
2026-03-17 15:18:23 +0500

    add

--- src/App.jsx
+++ src/App.jsx
@@ -69,12 +69,10 @@
 
 // export default App;
 
-
 import React, { lazy, Suspense } from "react";
 import { Routes, Route, useLocation } from "react-router-dom";
 import Leazy from "./components/leazy/Leazy";
 
-// Lazy pages
 const CatalogPage = lazy(() => import("./pages/catalogPage/CatalogPage"));
 const FilterResults = lazy(() => import("./pages/filterResults/FilterResults"));
 const Layout = lazy(() => import("./components/layout/Layout"));
@@ -115,7 +113,7 @@ class ErrorBoundary extends React.Component {
   }
 }
 
-const AppRoutes = () => {
+const App = () => {
   const location = useLocation();
 
   return (
@@ -146,95 +144,4 @@ const AppRoutes = () => {
   );
 };
 
-export default AppRoutes;
-
-
-// import React, { lazy, Suspense, useEffect, useState } from "react";
-// import { Routes, Route, useLocation } from "react-router-dom";
-// import { ToastContainer } from 'react-toastify';
-// import Leazy from "./components/leazy/Leazy";
-
-// const CatalogPage = lazy(() => import("./pages/catalogPage/CatalogPage"));
-// const FilterResults = lazy(() => import("./pages/filterResults/FilterResults"));
-// const Layout = lazy(() => import("./components/layout/Layout"));
-// const Home = lazy(() => import("./pages/home/Home"));
-// const Company = lazy(() => import("./pages/Company/Company"));
-// const Partner = lazy(() => import("./pages/partner/Partner"));
-// const Contact = lazy(() => import("./pages/Contact/Contact"));
-// const Razdel = lazy(() => import("./pages/razdel/Razdel"));
-// const Single = lazy(() => import("./pages/single/Single"));
-// const Result = lazy(() => import("./pages/result/Result"));
-// const FilterSearch = lazy(() => import("./pages/filterSearch/FilterSearch"));
-// const SearchProduct = lazy(() => import("./pages/searchProduct/SearchProduct"));
-// const Accardion = lazy(() => import("./pages/accardion/Accardion"));
-// const NewsPage = lazy(() => import("./pages/newsPage/NewsPage"));
-// const News = lazy(() => import("./pages/news/News"));
-// const NewsSingle = lazy(() => import("./pages/newsSingle/newsSingle"));
-// const NotFound = lazy(() => import("./pages/notfound/Notfound"));
-
-// class ErrorBoundary extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = { hasError: false };
-//   }
-//   static getDerivedStateFromError() {
-//     return { hasError: false };
-//   }
-//   componentDidCatch(error, info) {
-//     console.log("Error:", error, info);
-//   }
-//   render() {
-//     return this.props.children;
-//   }
-// }
-
-// const AppRoutes = () => {
-//   const location = useLocation();
-//   const [loading, setLoading] = useState(false);
-
-//   useEffect(() => {
-//     setLoading(true);
-//     const timer = setTimeout(() => setLoading(false), 500);
-//     return () => clearTimeout(timer);
-//   }, [location.pathname]);
-
-//   if (loading) return <Leazy />;
-
-//   return (
-//     <ErrorBoundary key={location.pathname}> 
-//       <Suspense fallback={<Leazy />}>
-//         <Routes>
-//           <Route element={<Layout />}>
-//             <Route path="/" element={<Home />} />
-//             <Route path="/razdel" element={<Razdel />} />
-//             <Route path="/company" element={<Company />} />
-//             <Route path="/partner" element={<Partner />} />
-//             <Route path="/contact" element={<Contact />} />
-//             <Route path="/single/:id" element={<Single />} />
-//             <Route path="/filter" element={<FilterSearch />} />
-//             <Route path="/news" element={<News />} />
-//             <Route path="/result" element={<Result />} />
-//             <Route path="/accardion" element={<Accardion />} />
-//             <Route path="/search" element={<SearchProduct />} />
-//             <Route path="/new" element={<NewsPage />} />
-//             <Route path="/filterResults" element={<FilterResults />} />
-//             <Route path="/:categoryName/:id" element={<CatalogPage />} />
-//             <Route path='/news-single/:id' element={<NewsSingle />} />
-//             <Route path="*" element={<NotFound />} />
-//           </Route>
-//         </Routes>
-//       </Suspense>
-//     </ErrorBoundary>
-//   );
-// };
-
-// const App = () => {
-//   return (
-//     <>
-//       <AppRoutes />
-//       <ToastContainer />
-//     </>
-//   );
-// };
-
-// export default App;
+export default App;
